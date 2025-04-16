0ab305065ca9bc9556bdc3761389db4452fb816d
manager63 <abdulkadirkqn@gmail.com>
2025-04-17 00:22:54 +0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <img src="/anka-logo.png" alt="Anka Logo" style={{ height: 100 }} />
+      <h1>BigStar Ürünleri</h1>
+      <ul>
+        <li>Yaşam Koçluğu - 1000 TL</li>
+        <li>Manevi Rehberlik - 1000 TL</li>
+        <li>Astral Seyahat Eğitimleri - 1000 TL</li>
+        <li>Mana Alemi Eğitimleri - 1000 TL</li>
+        <li>Yıldızname - 1000 TL</li>
+        <li>Kitaplar - 1000 TL</li>
+      </ul>
+      <p>Referans sistemi: Binary, 7 katman derinlik, %20 komisyon</p>
+    </div>
+  )
+}
